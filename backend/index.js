55234f61d721306ed205c6315a7b60be0ccd85a2
Rasmus <uusipaikka.rasmus@gmail.com>
2024-07-15 17:37:03 +0300

    port to config

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const app = require('./app')
+const config = require('./utils/config')
 
-const PORT = 3003
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT})`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT})`)
 })
