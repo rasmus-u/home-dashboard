1f0c13148a1040b26d5641e6ffff439e28a80f7d
Rasmus <uusipaikka.rasmus@gmail.com>
2024-07-16 11:16:59 +0300

    bikes to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getStations } from "./reducers/bikeReducer"
+
 const App = () => {
+  const stationIds = ['033', '070', '162']
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getStations(stationIds))
+  }, [])
+
+  const bikeStations = useSelector(state => state.bikeStations)
+
+  // console.log(bikeStations)
+
   return (
     <div className="bg-red-300 text-3xl text-white text-center">
-      Hello world
+      {bikeStations.map(station => {
+        return <p key={station.stationId}>{station.name} has {station.bikesAvailable} bikes</p>
+      })}
     </div>
   )
 }
