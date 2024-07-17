2cba1ebad979cb0580a4d151e0f7179c87e7e757
Rasmus <uusipaikka.rasmus@gmail.com>
2024-07-17 10:59:10 +0300

    controller for multiple stops

--- backend/controllers/stops.js
+++ backend/controllers/stops.js
@@ -74,4 +74,56 @@ stopRouter.get('/:id', async (request, response) => {
   }
 })
 
+stopRouter.get('/multipleStops/*', async (request, response) => {
+  // Multiple stops
+
+  const stopIdsPath = request.params[0]
+  const stopIds = stopIdsPath.split('/').filter(id => id)
+
+  if (stopIds.length > 5) {
+    // too many stops requested
+    response.status(400).send('Too many stops in query')
+  }
+
+  const stopQuery = (stopId, index) => `
+        stop${index}: stop(id: "${stopId}") {
+          name
+          vehicleMode
+          gtfsId
+          stoptimesWithoutPatterns(
+            timeRange: 1200 
+            omitCanceled: false 
+            numberOfDepartures: 10)
+          {
+            realtimeArrival
+            realtime
+            headsign
+            trip {
+              route {
+                shortName
+              }
+            }
+          }
+        }
+  `
+
+  const baseQuery = {
+    query: `
+      query {
+        ${stopIds.map((id, index) => stopQuery(id, index))}
+      }
+    `
+  }
+
+  try {
+    const apiResponse = await axios.post(apiUrl, baseQuery, headers)
+    const data = Object.values(apiResponse.data.data)
+    const stops = data.map(v => toObject(v))
+    response.status(200).json(stops)
+  } catch (error) {
+    console.error(error)
+    response.status(500).send('Internal server error.')
+  }
+})
+
 module.exports = stopRouter
