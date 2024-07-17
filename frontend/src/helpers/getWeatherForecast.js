fb7cfa67e8155f1f832c2ad75ca00e5005ec4eb0
Rasmus <uusipaikka.rasmus@gmail.com>
2024-07-17 23:13:33 +0300

    error bad request

--- frontend/src/helpers/getWeatherForecast.js
+++ frontend/src/helpers/getWeatherForecast.js
@@ -1,34 +1,30 @@
-import MetoLib from '@fmidev/metolib';
+import Metolib from '@fmidev/metolib';
 
 const SERVER_URL = 'https://opendata.fmi.fi/wfs';
 const STORED_QUERY_FORECAST =
   'fmi::forecast::hirlam::surface::point::multipointcoverage';
 
 const getWeatherForecast = async (area, duration, step) => {
-  const requestParser = new Metolib.WfsRequestParser();
-  return new Promise((resolve, reject) => {
-    if (connection.connect(SERVER_URL, STORED_QUERY_FORECAST)) {
-      // Connection was properly initialized. So, get the data.
-      requestParser.getData({
-        requestParameter: "temperature,windspeedms",
-        begin: new Date(),
-        end: new Date((new Date()).getTime() + 26 * 60 * 60 * 1000), //new Date((new Date()).getTime() + duration),
-        timestep: 60 * 60 * 1000,//step,
-        sites: ["Helsinki"],//[area], // Use the area parameter
-        callback: (data, errors) => {
-          connection.disconnect(); // Ensure disconnection in both cases
-          if (errors.length > 0) {
-            console.error(errors);
-            reject(errors); // Reject the promise on error
-          } else {
-            resolve(data); // Resolve the promise with data
-          }
+  const connection = new Metolib.WfsConnection();
+  if (connection.connect(SERVER_URL, STORED_QUERY_FORECAST)) {
+    // Connection was properly initialized. So, get the data.
+    connection.getData({
+      requestParameter: "temperature,windspeedms",
+      begin: new Date(),
+      end: new Date((new Date()).getTime() + 26 * 60 * 60 * 1000),
+      timestep: 60 * 60 * 1000,
+      sites: ["Helsinki", "Turku"],
+      callback: (data, errors) => {
+        connection.disconnect(); // Ensure disconnection in both cases
+        if (errors.length > 0) {
+          console.error(errors);
+          reject(errors); // Reject the promise on error
+        } else {
+          resolve(data); // Resolve the promise with data
         }
-      });
-    } else {
-      reject(new Error("Failed to connect to the server."));
-    }
-  });
+      }
+    });
+  }
 }
 
 export default getWeatherForecast;
