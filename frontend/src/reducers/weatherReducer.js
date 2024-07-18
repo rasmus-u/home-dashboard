fc3baed905b8f7593e9d0a959d764a63d8c4555d
Rasmus <uusipaikka.rasmus@gmail.com>
2024-07-18 11:59:38 +0300

    improved weather data format

--- frontend/src/reducers/weatherReducer.js
+++ frontend/src/reducers/weatherReducer.js
@@ -3,7 +3,27 @@ import { getWeatherForecast } from "../helpers/weather";
 
 const weatherSlice = createSlice({
   name: 'weather',
-  initialState: {},
+  initialState: {
+    city: '',
+    weatherData: {
+      temperature: {
+        values: [],
+        unit: ''
+      },
+      windspeedms: {
+        values: [],
+        unit: ''
+      },
+      totalcloudcover: {
+        values: [],
+        unit: ''
+      },
+      precipitationamount: {
+        values: [],
+        unit: ''
+      }
+    }
+  },
   reducers: {
     updateWeather(state, action) {
       return action.payload
@@ -19,20 +39,28 @@ export const getWeather = (area, duration, step) => {
       const forecastData = await getWeatherForecast(area, duration, step)
       const weatherData = forecastData.locations[0].data
       const city = forecastData.locations[0].info.name
-      for (const key in weatherData) {
-        if (weatherData.hasOwnProperty(key)) {
-          const weatherMetric = {
-            values: weatherData[key].timeValuePairs,
-            unit: forecastData.properties[key].unit
-          }
-          weatherData[key] = weatherMetric
+
+      const steps = weatherData.temperature.timeValuePairs
+      const weatherDataTypes = Object.keys(weatherData)
+
+      const weatherByTime = steps.map((timeSlot, index) => {
+        const weatherMetrics = {
+          time: timeSlot.time
         }
-      }
+        for (const key in weatherDataTypes) {
+          const weatherMetric = {
+            value: weatherData[weatherDataTypes[key]].timeValuePairs[index].value,
+            unit: forecastData.properties[weatherDataTypes[key]].unit
+          }
+          weatherMetrics[weatherDataTypes[key]] = weatherMetric
+        }
+        return weatherMetrics
+      })
+
       const weather = {
         city,
-        weatherData
+        weatherByTime
       }
-
       dispatch(updateWeather(weather))
     } catch (error) {
       console.error(error)
