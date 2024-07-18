19078dee2b332527af573afd8b9eece1b96bbb44
Rasmus <uusipaikka.rasmus@gmail.com>
2024-07-18 10:59:16 +0300

    fixed weatherreducer

--- frontend/src/reducers/weatherReducer.js
+++ frontend/src/reducers/weatherReducer.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import getWeatherForecast from "../helpers/getWeatherForecast";
+import { getWeatherForecast } from "../helpers/weather";
 
 const weatherSlice = createSlice({
   name: 'weather',
@@ -14,25 +14,9 @@ const weatherSlice = createSlice({
 export const { updateWeather } = weatherSlice.actions
 
 export const getWeather = (area, duration, step) => {
-  console.log('Getting weather forecast...')
   return async dispatch => {
-    // const weather = [
-    //   {
-    //     "temperature": 10,
-    //     "windspeedms": 5
-    //   },
-    //   {
-    //     "temperature": 9,
-    //     "windspeedms": 6
-    //   },
-    //   {
-    //     "temperature": 8,
-    //     "windspeedms": 7
-    //   }
-    // ]
     try {
       const forecastData = await getWeatherForecast(area, duration, step)
-      console.log('full data', forecastData)
       const locationData = forecastData.locations[0].data
       for (const key in locationData) {
         if (locationData.hasOwnProperty(key)) {
@@ -43,7 +27,6 @@ export const getWeather = (area, duration, step) => {
           locationData[key] = weatherMetric
         }
       }
-      console.log(locationData)
       dispatch(updateWeather(locationData))
     } catch (error) {
       console.error(error)
