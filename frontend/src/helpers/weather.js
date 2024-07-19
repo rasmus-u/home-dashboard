536e122de296537c91da25ccf5b53dc848f66cda
Rasmus <uusipaikka.rasmus@gmail.com>
2024-07-19 11:09:58 +0300

    helper for weather type

--- frontend/src/helpers/weather.js
+++ frontend/src/helpers/weather.js
@@ -4,7 +4,7 @@ const SERVER_URL = 'https://opendata.fmi.fi/wfs';
 const STORED_QUERY_FORECAST =
   'fmi::forecast::harmonie::surface::point::multipointcoverage';
 
-export const getWeatherForecast = async (area, duration, step) => {
+const getWeatherForecast = async (area, duration, step) => {
   const connection = new Metolib.WfsConnection();
   return new Promise((resolve, reject) => {
     if (connection.connect(SERVER_URL, STORED_QUERY_FORECAST)) {
@@ -29,3 +29,30 @@ export const getWeatherForecast = async (area, duration, step) => {
   }
   )
 }
+
+const getWeatherType = (weather) => {
+  const temperature = weather.temperature.value;
+  const windspeed = weather.windspeedms.value;
+  const cloudcover = weather.totalcloudcover.value;
+  const precipitation = weather.precipitationamount.value;
+
+  if (precipitation > 1) {
+    if (temperature < 0) {
+      return "SNOWING"
+    } else if (precipitation >= 3.5) {
+      return "POURING RAIN";
+    } else {
+      return "RAINING";
+    }
+  } else {
+    if (windspeed >= 10) {
+      return "WINDY"
+    } else if (cloudcover >= 80) {
+      return "CLOUDY";
+    } else {
+      return "SUNNY";
+    }
+  }
+}
+
+export { getWeatherForecast, getWeatherType }
