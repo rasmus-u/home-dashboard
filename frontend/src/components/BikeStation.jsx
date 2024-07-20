89f3b26f33c8c35522a1e791cb5ef5f9fc6b8153
Rasmus <uusipaikka.rasmus@gmail.com>
2024-07-20 18:38:33 +0300

    fixed ui and max bike bug

--- frontend/src/components/BikeStation.jsx
+++ frontend/src/components/BikeStation.jsx
@@ -1,7 +1,7 @@
 import bikeIcon from '../assets/bikeIcon.svg';
 
 const BikeStation = ({ bikeStopData }) => {
-  const percentageFull = Math.round(100 * bikeStopData.bikesAvailable / bikeStopData.spacesAvailable)
+  const percentageFull = Math.round(100 * bikeStopData.bikesAvailable / (bikeStopData.spacesAvailable + bikeStopData.bikesAvailable))
 
   return (
     <div className="flex flex-row h-20 justify-start items-center gap-3 bg-widgets rounded-xl px-4">
@@ -10,7 +10,7 @@ const BikeStation = ({ bikeStopData }) => {
         <div className="bg-alepa_yellow h-full" style={{ width: `${percentageFull}%` }} />
       </div>
       <div className='flex flex-row mx-2 gap-2 justify-end items-center flex-none'>
-        <p className='text-right font-semibold text-alepa_yellow text-3xl w-18'>{bikeStopData.bikesAvailable}/{bikeStopData.spacesAvailable}</p>
+        <p className='text-right font-semibold text-alepa_yellow text-3xl w-20'>{bikeStopData.bikesAvailable}/{bikeStopData.spacesAvailable + bikeStopData.bikesAvailable}</p>
         <img src={bikeIcon} alt="bike icon" className='w-9' />
       </div>
     </div>
